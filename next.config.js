28e0521353de86d709432e8d0ea0e54186bb0a52
yahia <yahyakawas88@gmail.com>
2024-05-13 12:32:01 +0300

    resolving conflict

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {}
-=======
 const nextConfig = {
     images:{
         remotePatterns: [{hostname: "images.unsplash.com"}],
@@ -10,6 +7,5 @@ const nextConfig = {
         serverActions: true,
     },
 };
->>>>>>> 974a4fa9e3bcb682a4b639c8e1ecd17511769a33
 
 module.exports = nextConfig
